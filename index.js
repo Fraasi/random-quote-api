c86aa0f8683ce54e763572fb1648850118829130
Fraasi <fraasi.gh@gmail.com>
2022-09-22 16:13:19 +0300

    cleanup old files

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')()
-const home = require('./routes/home.js')
-const authors = require('./routes/authors.js')
-
-app.use('/', home)
-app.use('/authors', authors)
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Serving at port: ' + port));
