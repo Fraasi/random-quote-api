0a512ea43015400968b868d5e7b0b041d8fa59cb
Fraasi <fraasi.gh@gmail.com>
2022-09-24 23:35:07 +0300

    delete help.route, add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import helmet from "helmet";
 // import { connect, set } from "mongoose";
 import { NODE_ENV, PORT, ORIGIN, CREDENTIALS } from "@config";
 // import { dbConnection } from "@databases";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import { Routes } from "@interfaces/routes.interface";
 import errorMiddleware from "./middlewares/error.middleware";
+import * as pgk from "../package.json";
 
 class App {
   public app: express.Application;
@@ -17,6 +20,7 @@ class App {
     this.port = PORT || 3000;
 
     // this.connectToDatabase();
+    this.initializeSwagger();
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
   }
@@ -42,11 +46,31 @@ class App {
   //   connect(dbConnection.url, dbConnection.options);
   // }
 
+  private initializeSwagger() {
+    const options = {
+      swaggerDefinition: {
+        info: {
+          title: "rq-api",
+          version: pgk.version,
+          description: "random quote api",
+          contact: {
+            name: "API Support",
+            url: pgk.bugs.url,
+          },
+        },
+      },
+      apis: ["swagger.yaml"],
+    };
+
+    const specs = swaggerJSDoc(options);
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+  }
+
   private initializeMiddlewares() {
     this.app.use(helmet());
     this.app.use(errorMiddleware);
-    // this.app.use(express.json());
-    // this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
   }
 
   private initializeRoutes(routes: Routes[]) {
